d6e6f30ed2ad45227728d6c5f20ace5cc5ee971c
LEMUEL CAMPOS MENEZES GOMES <lemuel0072004@gmail.com>
2023-03-01 21:47:30 -0300

    Criando repositório para o Projeto Jogo da Forca

---
+++ UC9_JOGO_DA_FORCA/Program.cs
@@ -0,0 +1,73 @@
+﻿int i, tentativas;
+int encontrado = 0;
+string letra = "";
+string[] palavra_secreta = new string[10];
+string[] palavra_visivel = new string[10];
+string jogador1;
+string jogador2;
+
+for (i = 0; i <= 9; i++)
+{
+    Console.WriteLine((i + 1) + "ª Letra da palavra (total de 10 letras)");
+    palavra_secreta[i] = Console.ReadLine();
+}
+
+for (i = 0; i <= 9; i++)
+{
+    Console.Write(palavra_secreta[i] + " ");
+}
+    Console.WriteLine("");
+    Console.writeLine(" Infortme o nome do jogador 1: ")
+    jogador1 = Console.ReadLine();
+    Console.WriteLine("");
+    Console.WriteLine("");
+    Console.writeLine(" Infortme o nome do jogador 2: ")
+    jogador2 = Console.ReadLine();
+    Console.WriteLine("");
+    Console.WriteLine("Pressione ENTER para continuar...");
+    Console.ReadLine();
+
+
+Console.Clear();
+
+tentativas = 0;
+
+while (tentativas < 10)
+{
+    // SEMPRE MOSTRAR A PALAVRA EM JOGO NA TELA...
+    Console.Clear();
+    for (i = 0; i <= 9; i++)
+    {
+        Console.Write(palavra_visivel[i] + " ");
+    }
+    Console.WriteLine("");
+    Console.WriteLine("Você já ERROU " + tentativas + " vezes! (Máximo de 10 ERROS!)");
+    Console.WriteLine("");
+
+    // Usuário informa uma LETRA para tentativa de acerto...
+    Console.WriteLine("Qual LETRA deseja tentar?");
+    letra = Console.ReadLine();
+
+    // JOGO verifica se a LETRA informada EXISTE na Palavra Secreta...
+    for (i = 0; i < 10; i++)
+    {
+        // Se LETRA existe, JOGO mostra a LETRA na tela e na posição para o usuário;
+        if (letra == palavra_secreta[i])
+        {
+            palavra_visivel[i] = letra;
+            encontrado = 1;
+        }
+    }
+    // Se LETRA não existe, JOGO informa Letra Incorreta!
+    if (encontrado == 0)
+    {
+        Console.WriteLine("LETRA não encontrada!");
+        tentativas++;
+    }
+
+    encontrado = 0;
+
+    Console.WriteLine("");
+    Console.WriteLine("Pressione ENTER para continuar...");
+    Console.ReadLine();
+}
